ee5d22a43fac575d76b54e321fc35577759fcf49
des1gnerw1tch <zachcanon@gmail.com>
2021-11-26 00:51:10 -0500

    windows part 1

--- ProceduralTerrain/Assets/GrowMesh.cs
+++ ProceduralTerrain/Assets/GrowMesh.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 // Grows a mesh taller by changing its Y scale and adjusting position so it just looks like it grows
 public class GrowMesh : MonoBehaviour {
+	[SerializeField] private Window [] windows;
+	[SerializeField] private float windowSeperation;
+	private float gainedHeight = 0;
 
 	// Grows the Y scale of this object bottom up
 	public void GrowScaleY (float x) {
@@ -14,5 +17,19 @@ public class GrowMesh : MonoBehaviour {
 		pos.y += x / 2; // from my experiments
 		this.transform.localScale = scale;
 		this.transform.position = pos;
+		gainedHeight = x;
+		PlaceNewWindows ();
+	}
+
+	// Places windows on a building
+	void PlaceNewWindows () {
+		for (float i = windowSeperation; i < gainedHeight; i += windowSeperation) {
+			foreach (Window window in windows) {
+				GameObject newWindow = Instantiate (window.gameObject);
+				Vector3 pos = window.transform.position;
+				pos.y += i;
+				newWindow.transform.position = pos;
+			}
+		}
 	}
 }
---
+++ ProceduralTerrain/Assets/Window.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+// A window on a building... will randomize to whether this window is turned on or not
+/// </summary>
+public class Window : MonoBehaviour {
+	[SerializeField] private Material onMaterial;
+	[SerializeField] private Material offMaterial;
+	[SerializeField] private float percentWindowsOn;
+
+	// Start is called before the first frame update
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
